8c8b62ad5aad813d2ab525e40a432647f6935730
LukemansfieldPAX <90900950+LukemansfieldPAX@users.noreply.github.com>
2023-03-13 09:00:32 +0000

    Add project files.

---
+++ whizzy-software-media-organiser-LM/Form1.cs
@@ -0,0 +1,10 @@
+namespace whizzy_software_media_organiser_LM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
