93d0e0afc601e2a52ac6f9770518dd629837f01d
Lukemansfield1997 <90900950+LukeMansfield1997@users.noreply.github.com>
2023-03-13 15:30:46 +0000

    added UnitTest for mediaFiles assigned to playlist

---
+++ whizzy-software-media-organiser-Tests/MediaFileTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using whizzy_software_media_organiser_LM.Services;
+
+namespace whizzy_software_media_organiser_Tests
+{
+    public class MediaFileTests
+    {
+        PlaylistServiceJsonDataStore _playlistService;
+        [SetUp]
+        public void Setup()
+        {
+            _playlistService = new PlaylistServiceJsonDataStore();
+        }
+
+        [Test]
+        public void MediaFileIsAddedToPlaylist()
+        {
+            //Arrange
+            string playlistName = "new playlist";
+            string mediaFile = "C:\\Users\\Luke Mansfield\\Downloads\\sample.mp3";
+            string mediafileWithoutExtension = Path.GetFileNameWithoutExtension(mediaFile);
+            string mediaFileType = Path.GetExtension(mediaFile);
+
+            var playlist = _playlistService.CreatePlaylist(playlistName);
+
+            //Act
+            _playlistService.AddMediaFileToPlaylist(playlist.PlayListID, mediaFile);
+            //Assert
+            Assert.That(playlist.MediaFileItems.Count, Is.EqualTo(1));
+            Assert.That(playlist.MediaFileItems[0].Song, Is.EqualTo(mediafileWithoutExtension));
+            Assert.That(playlist.MediaFileItems[0].FilePath, Is.EqualTo(mediaFile));
+            Assert.That(playlist.MediaFileItems[0].FileType, Is.EqualTo(mediaFileType));
+        }
+
+    }
+}
